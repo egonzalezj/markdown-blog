2e9dc8d486991160b14b915b13c1b466f117efea
Elisabet Gonzalez <eli.jvn@gmail.com>
2021-04-26 18:45:24 -0500

    Finished project

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
+
+mongoose.connect('mongodb://localhost/blog', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.use(express.urlencoded({
+    extended: false
+}))
+
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({
+        createdAt: 'desc'
+    })
+    res.render('articles/index', {articles: articles})
 })
 
-app.listen (5000)
+app.use('/articles', articleRouter)
+
+app.listen(5000)
